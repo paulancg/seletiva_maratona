22c5e32755a6c780be17517a19518ddcd7176d12
paulancg <161087458+paulancg@users.noreply.github.com>
2025-11-07 23:16:22 -0300

    Update interesting_drink.cpp

--- homework2/interesting_drink.cpp
+++ homework2/interesting_drink.cpp
@@ -26,42 +26,11 @@ int main(){
     for (int i = 0; i < q; i++){
         
         int m; cin >> m;
-        
+        int valor = upper_bound(vec.begin(), vec.end(), m) - vec.begin();
+        cout << valor << '\n';
         
     }
 
-/**
 
-    bool Binary_Search(int n, int x, int a[]){
-        
-        int left = 0;
-        int right = n - 1:
-        
-        while (left <= right){
-            
-            int mid = (left + right)/2;
-            
-            if (a[mid] < x){
-                
-                left = mid + 1;
-                
-            }
-            
-            else if (a[mid] > x){
-                
-                right = mid - 1;
-                
-            }
-            
-            else{
-                
-                return true;
-            
-            }
-        }
-        return false;
-        
-    }
-**/    
     return 0;
 }
